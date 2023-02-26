c86e1ec8d16fe21c54f614e0b72dc50077fbf080
tuanpa.tpa <tuanpa.tpa@gmail.com>
2023-02-26 22:54:56 +0700

    deploy

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,23 +1,54 @@
-import { ethers } from "hardhat";
+import { ethers, hardhatArguments } from 'hardhat';
+import * as Config from './config';
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
+    await Config.initConfig();
+    const network = hardhatArguments.network ? hardhatArguments.network : 'dev';
+    const [deployer] = await ethers.getSigners();
+    console.log('deploy from address: ', deployer.address);
 
-  const lockedAmount = ethers.utils.parseEther("1");
+    const Floppy = await ethers.getContractFactory("Floppy");
+    const floppy = await Floppy.deploy();
+    console.log('Floppy address: ', floppy.address);
+    Config.setConfig(network + '.Floppy', floppy.address);
 
-  const Lock = await ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
+    const Vault = await ethers.getContractFactory("Vault");
+    const vault = await Vault.deploy();
+    console.log('Floppy address: ', vault.address);
+    Config.setConfig(network + '.Vault', vault.address);
+    
+    // const Floppy = await ethers.getContractFactory("USDT");
+    // const floppy = await Floppy.deploy();
+    // console.log('USDT address: ', floppy.address);
+    // Config.setConfig(network + '.USDT', floppy.address);
 
-  await lock.deployed();
+    // const Ico = await ethers.getContractFactory("FLPCrowdSale");
+    // const ico = await Ico.deploy(1000,100,'0xdF8De3b50Be87dE8676c4731187c5DC5C00E70F3', '0xd54D6d5BD983a6cA18F8820f80E0A970FE4A9a8c');
+    // console.log('ICO address: ', ico.address);
+    // Config.setConfig(network + '.ico', ico.address);
 
-  console.log(`Lock with 1 ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`);
+    
+    // const Hero = await ethers.getContractFactory("Hero");
+    // const hero = await Hero.deploy();
+    // console.log('stman hero address: ', hero.address);
+    // Config.setConfig(network + '.Hero', hero.address);
+
+
+    // const MKP = await ethers.getContractFactory("HeroMarketplace");
+    // const heroMarketplace = await MKP.deploy("0x65f00a282A58B30f8376D41832d76CeCB7b6186C", "0xd54D6d5BD983a6cA18F8820f80E0A970FE4A9a8c");
+    // console.log('Market deployed at: ', heroMarketplace.address);
+    
+    // const Auction = await ethers.getContractFactory("Auction");
+    // const auction = await Auction.deploy("0xd54D6d5BD983a6cA18F8820f80E0A970FE4A9a8c", "0x65f00a282A58B30f8376D41832d76CeCB7b6186C");
+    // console.log('Market deployed at: ', auction.address);
+    // Config.setConfig(network + '.Auction', auction.address);
+
+    await Config.updateConfig();
+    
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
+main().then(() => process.exit(0))
+    .catch(err => {
+        console.error(err);
+        process.exit(1);
 });
