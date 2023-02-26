bc8c0d1eab1a4d7ca766406d37917df0d5795287
tuanpa.tpa <tuanpa.tpa@gmail.com>
2023-02-26 22:16:43 +0700

    create contract

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,11 +1,13 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import * as dotenv from "dotenv";
+dotenv.config({path: __dirname + "/.env"});
 
 const config: HardhatUserConfig = {
   solidity: "0.8.17",
   networks: {
     bsctest: {
-      url: "",
+      url: "https://data-seed-prebsc-1-s1.binance.org:8545/",
       accounts: [process.env.PRIV_KEY]
     }
   },
